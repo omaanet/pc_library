403b1a4e900db3ceba607e9fa9b00006291d9d06
Oscar Mucchiati <info@omaa.it>
2025-03-13 15:23:13 +0100

    FIX: nextConfig for images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.vercel.app',
+                pathname: '/api/covers/**',
+            },
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                pathname: '/api/covers/**',
+            },
+        ],
         dangerouslyAllowSVG: true,
         contentDispositionType: 'attachment',
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
